3090cea9cbea0f240fdffc94d8f4eff478060d74
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-05-17 19:33:13 -0600

    updated

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// Stretch Goal 
+router.get("/", function (req, res, next) {
+  res.render("users", { title: "Users" });
 });
+
+router.get('/myname', function(req, res, next) {
+  res.send('Leah');
+});
+
+router.get('/myfavoritemovies', function(req, res, next) {
+  res.json(["Dirty Dancing", "Tenet", "Selena"]);
+});
+
 
 module.exports = router;
